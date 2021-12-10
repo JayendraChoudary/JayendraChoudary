2060ec4b200a59b2a8aa2df32043c9f78ccc14c1
JayendraChoudary <95288894+JayendraChoudary@users.noreply.github.com>
2021-12-10 12:59:19 +0530

    Add files via upload

---
+++ tofindweatheranumberisdivisiblebyboth3$5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+	int a;
+	printf("Enter the Value of A");
+	scanf("%d",&a);
+	if (a%3==0,a%5==0)
+	{
+		printf("The Enterd Value Is Divisible By both 3&5%d",a);
+	}
+	else
+	{
+		printf("The Enterd Value Is Not Divisible By Both 3&5=%d",a);
+	}
+}
